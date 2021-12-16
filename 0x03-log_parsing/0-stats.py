84213c1eb454c3ba7e2c94b4d4a9596114de2bf0
Andrew Mukare <dremukare@gmail.com>
2021-12-16 21:54:14 +0000

    attempted solution

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''reads stdin line by line and computes metrics'''
+import re
+import sys
+import signal
+
+
+toMatch = re.compile(
+                     r'^\d{1,3}\.\d{1,3}\.\d{1,3} \
+                     \.\d{1,3}\s\-\s\[[0-9]{4}\-[0-9] \
+                     {1,2}\-[0-9]{1,2}\s[0-9]{1,2}\: \
+                     [0-9]{1,2}\:[0-9]{1,2}.[0-9]{1,6}\] \
+                     \s\"GET\s\/projects\/260\sHTTP\/ \
+                     1\.1\"\s\d{3}\s\d{1,4}$')
+statusCodeTracker = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+fileSizeTracker = 0
+lineCount = 0
+
+
+def handler():
+    return True
+
+
+for line in sys.stdin:
+    lineCount += 1
+    if toMatch.match(line) is False:
+        continue
+    withoutDash = line.replace('-', '')
+    arrayFromString = withoutDash.split(' ')
+    try:
+        if arrayFromString[5] and isinstance(int(arrayFromString[5]), int):
+            statusCodeTracker[arrayFromString[5]] += 1
+        fileSizeTracker += int(arrayFromString[6])
+        if lineCount == 10 or signal.signal(signal.SIGINT, handler):
+            print('File size: {}'.format(fileSizeTracker))
+            for key, value in statusCodeTracker.items():
+                if int(arrayFromString[5]) not in statusCodeTracker.keys():
+                    continue
+                print('{}: {}'.format(key, value))
+    except:
+        pass
